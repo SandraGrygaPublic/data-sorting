39f93aff5717e10093b271c1ee0310e445c05c5e
SandraGrygaPublic <67583062+SandraGrygaPublic@users.noreply.github.com>
2020-06-30 12:19:26 +0200

    Fix incorrect spelling in generate-and-sort-file

--- app/scripts/generate-and-sort-file.sh
+++ app/scripts/generate-and-sort-file.sh
@@ -2,7 +2,7 @@
 set -exo pipefail
 
 usage() {
-    echo "generate-and-sort-file [TEST_FILE_SIZE_MB] [CHANK_MAX_SIZE_MB]"
+    echo "generate-and-sort-file [TEST_FILE_SIZE_MB] [CHUNK_MAX_SIZE_MB]"
 }
 
 if [[ "$#" != 2 ]]; then
@@ -11,14 +11,14 @@ if [[ "$#" != 2 ]]; then
 fi
 
 TEST_FILE_SIZE_MB="$1"
-CHANK_MAX_SIZE_MB="$2"
+CHUNK_MAX_SIZE_MB="$2"
 
 TEST_FILE_NAME="test_file"
 OUTPUT_FILE="test_result"
 
 TEST_FILE_SIZE=$((1024*1024*${TEST_FILE_SIZE_MB}))
-CHANK_MAX_SIZE=$((1024*1024*${CHANK_MAX_SIZE_MB}))
+CHUNK_MAX_SIZE=$((1024*1024*${CHUNK_MAX_SIZE_MB}))
 MAX_WORD_LENGHT=30
 
 file_generator.py --output_file $TEST_FILE_NAME --output_file_size $TEST_FILE_SIZE  --max_word_length $MAX_WORD_LENGHT
-sorting_script.py --input_file $TEST_FILE_NAME --output_file $OUTPUT_FILE --chunk_max_size $CHANK_MAX_SIZE
+sorting_script.py --input_file $TEST_FILE_NAME --output_file $OUTPUT_FILE --chunk_max_size $CHUNK_MAX_SIZE
